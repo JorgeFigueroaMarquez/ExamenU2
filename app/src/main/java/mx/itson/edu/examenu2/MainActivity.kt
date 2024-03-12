397d2c4a1225529c2565113f9d4b4bff76774ab3
Jorge Andrés Figueroa Márquez <jorge.figueroa228692@potros.itson.edu.mx>
2024-03-12 16:23:33 -0700

    first commit

---
+++ app/src/main/java/mx/itson/edu/examenu2/MainActivity.kt
@@ -0,0 +1,19 @@
+package mx.itson.edu.examenu2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.btn_visitar)
+        button.setOnClickListener {
+            var intent: Intent = Intent(this, MenuPrincipal::class.java)
+            startActivity(intent)
+        }
+    }
+}
